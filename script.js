5d57c330fd7bf98fce811eb2be48c73d5ef0f7bd
Miamurick <81481920+JanisLovcevics@users.noreply.github.com>
2026-02-11 22:02:08 +0200

    FLex input

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-enlarge_gallery_button = document.getElementById("enlarge_gallery");
+enlarge_gallery_button = document.getElementById("gallery_section").children[3];
+enlarge_specials_button = document.getElementById("specials_section").children[3];
 
 enlarge_gallery_button.addEventListener("click",  addImage)
-
+enlarge_specials_button.addEventListener("click", addImageSpecials)
 function addImage() {
-    image_link = document.getElementById("image_url_input").value;
+    image_link = document.getElementById("image_url_input_gallery").value;
+    if (image_link !== null && image_link !== "") {
+        new_image = document.createElement("img");
+        new_image.src = image_link;
+        new_image.classList.add("scaling_object");
+        document.getElementById("gallery_section").children[1].appendChild(new_image);
+    }
+}
+
+function addImageSpecials() {
+    image_link = document.getElementById("image_url_input_specials").value;
     if (image_link !== null && image_link !== "") {
         new_image = document.createElement("img");
         new_image.src = image_link;
